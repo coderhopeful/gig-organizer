a992f56435881e844183a4ee121e60c2e0f81dce
Arjun <emailarjunmuraly@gmail.com>
2022-08-15 14:19:54 +0530

    port error corrected

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config();
-const gigsSchema= require("./models/gigs")
-const cors= require ('cors')
+const gigsSchema = require("./models/gigs")
+const cors = require('cors')
 
-const app=express();
+const app = express();
 
 app.use(express.json())
 
-const PORT= process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 
-const gigRoute= require('./routes/gigs')
+const gigRoute = require('./routes/gigs')
 
 mongoose.connect(process.env.DB_CONNECT)
-.then(()=>console.log("DB online"))
-.catch(err=>console.log(err))
+  .then(() => console.log("DB online"))
+  .catch(err => console.log(err))
 
 
-app.use('/',gigRoute);
+app.use('/', gigRoute);
 
 app.use(express.static(path.join(__dirname, "/client")));
 
@@ -30,7 +30,7 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(PORT, ()=> console.log("Server Online") );
+app.listen(PORT, () => console.log("Server Online"));
 
 
 
